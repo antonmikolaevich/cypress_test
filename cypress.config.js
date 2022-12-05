a8f4fa596a0d6bd210a5f263e57d2783993f21f8
Anton Rak <Anton_Rak@epam.com>
2022-12-05 15:01:22 +0300

    delete the mochaawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e:{
   baseUrl: 'https://www.saucedemo.com/',
-  setupNodeEvents(on, config) {
-    require('cypress-mochawesome-reporter/plugin')(on);
-  }
   },
   chromeWebSecurity: false,
   reporter: "cypress-junit-reporter",
