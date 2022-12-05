e1064062132428a0f2276c0f6540636e554ec5e9
Anton Rak <Anton_Rak@epam.com>
2022-12-05 21:28:54 +0300

    add multi-reporters

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,28 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin');
 
-module.exports = defineConfig({
+module.exports = defineConfig(
+  {
   e2e:{
-  baseUrl: 'https://www.saucedemo.com/',
+  setupNodeEvents(on, config) {
+    return registerReportPortalPlugin(on, config);
+  }
   },
   chromeWebSecurity: false,
-  reporter: "cypress-junit-reporter",
+  reporter: "cypress-multi-reporters",
   reporterOptions: {
-    mochaFile: "cypress/results/results.xml",
-    toConsole: true
+    reporterEnabled: "cypress-junit-reporter, @reportportal/agent-js-cypress",
+    cypressJunitReporterReporterOptions: {
+      mochaFile: "cypress/results/results.xml",
+      toConsole: true
+    },
+    reportportalAgentJsCypressReporterOptions: {
+      endpoint: 'https://reportportal.epam.com/api/v1',
+      token: '6dbbd2b0-2395-48ff-adab-63209b916b35',
+      launch: 'anton_rak_TEST_EXAMPLE',
+      project: 'anton_rak_personal',
+      description: 'cypress_test_project'
+    }
   }
-  
-})
+}
+)
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@reportportal/agent-js-cypress/lib/commands/reportPortalCommands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
